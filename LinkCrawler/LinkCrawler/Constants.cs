989ca7cede65547dc2926268ab4cdc56d6376a1a
Henrik Molnes <molnes@gmail.com>
2016-02-03 19:09:37 +0100

    added constants

---
+++ LinkCrawler/LinkCrawler/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinkCrawler
+{
+    public class Constants
+    {
+        public class AppSettings
+        {
+            public const string BaseUrl = "BaseUrl";
+        }
+
+        public class LinkAttributes
+        {
+            public const string Mailto = "mailto";
+            public const string Href = "href";
+            public const string LinkSearchPattern = "//a[@href]";
+        }
+    }
+}
