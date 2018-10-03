473058f42529d31f7cf3560e7b65ee0f723f5402
David Tolan <tolan.david@gmail.com>
2018-10-02 22:34:52 -0500

    Added autofixture packages, added SlackOutput tests

---
+++ LinkCrawler/LinkCrawler.Tests/UtilsTests/OutputsTests/SlackOutputTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using NUnit.Framework;
+using LinkCrawler.Utils.Outputs;
+using AutoFixture.AutoMoq;
+using LinkCrawler.Utils.Clients;
+using LinkCrawler.Models;
+using Moq;
+
+namespace LinkCrawler.Tests.UtilsTests.OutputsTests
+{
+    [TestFixture]
+    public class SlackOutputTests
+    {
+        private Fixture _fixture;
+
+        [SetUp]
+        public void SetUup()
+        {
+            _fixture = new Fixture();
+            _fixture.Customize(new AutoMoqCustomization());
+        }
+
+        [Test]
+        public void WriteError()
+        {
+            
+            var slackClient = _fixture.Freeze<Mock<ISlackClient>>();
+            var slackOutput = _fixture.Freeze<SlackOutput>();
+            var responseModel = _fixture.Create<IResponseModel>();
+
+            slackClient.Setup(x => x.NotifySlack(responseModel));
+
+            slackOutput.WriteError(responseModel);
+
+            slackClient.VerifyAll();
+
+        }
+    }
+}
