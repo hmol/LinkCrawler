e03559dc3793b6d273067440fd30754644f6a004
Jose Montanez <josemontanez1992@gmail.com>
2022-11-04 16:16:15 -0600

    Unit test issue 44
    Created a unit test to check valid URL pattern.

---
+++ LinkCrawler/LinkCrawler.Tests/UtilsTests/ExtensionsTests/RegexExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using LinkCrawler.Utils.Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkCrawler.Tests.UtilsTests.ExtensionsTests
+{
+    [TestFixture]
+    public class RegexExtensionsTest
+    {
+        public void IsNotMatch_Should_Return_False()
+        {
+            
+            string regex = "(^http[s]?:\\/{2})|(^www)|(^\\/{1,2})";
+            string url = "website.com:///podcast/";
+            bool expression = RegexExtensions.IsNotMatch(new System.Text.RegularExpressions.Regex(regex), url);
+            Assert.IsFalse(expression);
+        }
+    }
+}
